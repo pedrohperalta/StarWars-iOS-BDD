503dd7bf223bdc9084da0c586ad6b39a109f4df8
Pedro Henrique Prates Peralta <pedro@ckl.io>
2016-05-15 15:01:30 -0300

    Creates person entity

---
+++ StarWars/Entities/Person.swift
@@ -0,0 +1,14 @@
+//
+// Created by Pedro Henrique Prates Peralta on 5/15/16.
+// Copyright (c) 2016 Pedro Henrique Peralta. All rights reserved.
+//
+
+import Foundation
+
+
+class Person {
+    var name: String?
+    var gender: String?
+    var height: Int?
+    var mass: Int?
+}
